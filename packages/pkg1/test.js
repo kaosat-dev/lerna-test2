9861b58cbf11de68d7d565d88179939a62933163
Mark Moissette <kaosat.dev@gmail.com>
2017-09-18 12:26:29 +0200

    test(castle): added test for castle() function (#1)
    * test(castle): added test for castle() function
    * test(pkg): added correct test command in root package
    * test(CI): updated travis config to only run on Node > 6

--- packages/pkg1/test.js
+++ packages/pkg1/test.js
@@ -1,10 +1,18 @@
 const test = require('tape')
-const {fun} = require('./index')
+const {fun, castle} = require('./index')
 
-test('simple test for pkg1', function (t) {
+test('simple test for pkg1: fun()', function (t) {
   t.plan(1)
 
   const expected = 42
   const observed = fun()
+  t.equal(expected, observed)
+})
+
+test('simple test for pkg1: castle()', function (t) {
+  t.plan(1)
+
+  const expected = 'oh yeah let us build casstlye'
+  const observed = castle()
   t.equal(expected, observed)
 })
